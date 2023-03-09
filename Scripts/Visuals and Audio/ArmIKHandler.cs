e2459fcd883a25f168c4e81f9d3f0d961ca9a590
Farrukh Kamal <farrukhkamal429@gmail.com>
2023-03-09 06:12:22 +0500

    no need for independant ik arms

--- Scripts/Visuals and Audio/ArmIKHandler.cs
+++ Scripts/Visuals and Audio/ArmIKHandler.cs
@@ -4,90 +4,90 @@ using UnityEngine;
 
 public class ArmIKHandler : MonoBehaviour
 {
-    [SerializeField] private List<IKLerp> R_locations = new List<IKLerp>();
-    [SerializeField] private List<IKLerp> L_locations = new List<IKLerp>();
-    [SerializeField] private float lerpSpeedScale = 100f;
-    [SerializeField] private GunManager gunManager;
+    // [SerializeField] private List<IKLerp> R_locations = new List<IKLerp>();
+    // [SerializeField] private List<IKLerp> L_locations = new List<IKLerp>();
+    // [SerializeField] private float lerpSpeedScale = 100f;
+    // [SerializeField] private GunManager gunManager;
 
-    private List<Transform> R_targetTransforms = new List<Transform>();
-    private List<Transform> L_targetTransforms = new List<Transform>();
+    // private List<Transform> R_targetTransforms = new List<Transform>();
+    // private List<Transform> L_targetTransforms = new List<Transform>();
 
-    Transform R_current_T_toMove;
-    Transform R_current_T_target;
+    // Transform R_current_T_toMove;
+    // Transform R_current_T_target;
 
-    Transform L_current_T_toMove;
-    Transform L_current_T_target;
+    // Transform L_current_T_toMove;
+    // Transform L_current_T_target;
     
-    void Start()
-    {
-        SetupTargets();
-    }
+    // void Start()
+    // {
+    //     SetupTargets();
+    // }
     
-    void Update()
-    {
-        for (int i = 0; i < R_locations.Count-1; i++)
-        {
-            // R_targetTransforms[i] = GunManager.currentgun.R_targetTransforms[i];
-            // L_targetTransforms[i] = GunManager.currentgun.L_targetTransforms[i];
-            // R_targetTransforms.Add(GunManager.currentgun.R_targetTransforms[i]);
-            // L_targetTransforms.Add(GunManager.currentgun.L_targetTransforms[i]);
+    // void Update()
+    // {
+    //     for (int i = 0; i < R_locations.Count-1; i++)
+    //     {
+    //         // R_targetTransforms[i] = GunManager.currentgun.R_targetTransforms[i];
+    //         // L_targetTransforms[i] = GunManager.currentgun.L_targetTransforms[i];
+    //         // R_targetTransforms.Add(GunManager.currentgun.R_targetTransforms[i]);
+    //         // L_targetTransforms.Add(GunManager.currentgun.L_targetTransforms[i]);
 
-            // for right arm
+    //         // for right arm
 
-            R_current_T_toMove = R_locations[i].transformToMove;
-            R_current_T_target = R_targetTransforms[i];
+    //         R_current_T_toMove = R_locations[i].transformToMove;
+    //         R_current_T_target = R_targetTransforms[i];
 
-            // lerping Positions
-            // R_current_T_toMove.position = Vector3.Lerp(R_current_T_toMove.position, R_current_T_target.position,
-            //         R_locations[i].lerpSpeeds.x * lerpSpeedScale * Time.deltaTime);
+    //         // lerping Positions
+    //         // R_current_T_toMove.position = Vector3.Lerp(R_current_T_toMove.position, R_current_T_target.position,
+    //         //         R_locations[i].lerpSpeeds.x * lerpSpeedScale * Time.deltaTime);
 
-            // // lerping Rotations
-            // R_current_T_toMove.rotation = Quaternion.Slerp(R_current_T_toMove.rotation, R_current_T_target.rotation,
-            //         R_locations[i].lerpSpeeds.y * lerpSpeedScale * Time.deltaTime);
+    //         // // lerping Rotations
+    //         // R_current_T_toMove.rotation = Quaternion.Slerp(R_current_T_toMove.rotation, R_current_T_target.rotation,
+    //         //         R_locations[i].lerpSpeeds.y * lerpSpeedScale * Time.deltaTime);
            
-            R_current_T_toMove.position = R_current_T_target.position;
-            R_current_T_toMove.rotation = R_current_T_target.rotation;
+    //         R_current_T_toMove.position = R_current_T_target.position;
+    //         R_current_T_toMove.rotation = R_current_T_target.rotation;
 
 
-            // for left arm
+    //         // for left arm
 
-            L_current_T_toMove = L_locations[i].transformToMove;
-            L_current_T_target = L_targetTransforms[i];
+    //         L_current_T_toMove = L_locations[i].transformToMove;
+    //         L_current_T_target = L_targetTransforms[i];
 
-            // lerping Positions
-            // L_current_T_toMove.position = Vector3.Lerp(L_current_T_toMove.position, L_current_T_target.position,
-            //         L_locations[i].lerpSpeeds.x * lerpSpeedScale * Time.deltaTime);
+    //         // lerping Positions
+    //         // L_current_T_toMove.position = Vector3.Lerp(L_current_T_toMove.position, L_current_T_target.position,
+    //         //         L_locations[i].lerpSpeeds.x * lerpSpeedScale * Time.deltaTime);
 
-            // // lerping Rotations
-            // L_current_T_toMove.rotation = Quaternion.Slerp(L_current_T_toMove.rotation, L_current_T_target.rotation,
-            //         L_locations[i].lerpSpeeds.y * lerpSpeedScale * Time.deltaTime);
+    //         // // lerping Rotations
+    //         // L_current_T_toMove.rotation = Quaternion.Slerp(L_current_T_toMove.rotation, L_current_T_target.rotation,
+    //         //         L_locations[i].lerpSpeeds.y * lerpSpeedScale * Time.deltaTime);
 
-            L_current_T_toMove.position = L_current_T_target.position;
-            L_current_T_toMove.rotation = L_current_T_target.rotation;
-        }
-    }
+    //         L_current_T_toMove.position = L_current_T_target.position;
+    //         L_current_T_toMove.rotation = L_current_T_target.rotation;
+    //     }
+    // }
     
-    void SetupTargets()
-    {
-        for (int i = 0; i < R_locations.Count-1; i++)
-        {
-            // R_targetTransforms[i] = GunManager.currentgun.R_targetTransforms[i];
-            // L_targetTransforms[i] = GunManager.currentgun.L_targetTransforms[i];
-            R_targetTransforms.Add(GunManager.currentgun.R_targetTransforms[i]);
-            L_targetTransforms.Add(GunManager.currentgun.L_targetTransforms[i]);
-        }     
-    }
+    // void SetupTargets()
+    // {
+    //     for (int i = 0; i < R_locations.Count-1; i++)
+    //     {
+    //         // R_targetTransforms[i] = GunManager.currentgun.R_targetTransforms[i];
+    //         // L_targetTransforms[i] = GunManager.currentgun.L_targetTransforms[i];
+    //         R_targetTransforms.Add(GunManager.currentgunSettings.R_targetTransforms[i]);
+    //         L_targetTransforms.Add(GunManager.currentgunSettings.L_targetTransforms[i]);
+    //     }     
+    // }
     
-    public void updateTargets()
-    {
-        for (int i = 0; i < R_locations.Count-1; i++)
-        {
-            R_targetTransforms[i] = GunManager.currentgun.R_targetTransforms[i];
-            L_targetTransforms[i] = GunManager.currentgun.L_targetTransforms[i];
-            // R_targetTransforms.Add(GunManager.currentgun.R_targetTransforms[i]);
-            // L_targetTransforms.Add(GunManager.currentgun.L_targetTransforms[i]);
-        }     
-    }
+    // public void updateTargets()
+    // {
+    //     for (int i = 0; i < R_locations.Count-1; i++)
+    //     {
+    //         R_targetTransforms[i] = GunManager.currentgunSettings.R_targetTransforms[i];
+    //         L_targetTransforms[i] = GunManager.currentgunSettings.L_targetTransforms[i];
+    //         // R_targetTransforms.Add(GunManager.currentgun.R_targetTransforms[i]);
+    //         // L_targetTransforms.Add(GunManager.currentgun.L_targetTransforms[i]);
+    //     }     
+    // }
 }
 [System.Serializable]
 public struct IKLerp
